f6e84617404041cc3a8291bdac8570cad47569fb
vas59 <vasily.yuryev.a1@gmail.com>
2019-08-12 13:18:58 +0300

    namespace

--- src/serial_port.cpp
+++ src/serial_port.cpp
@@ -1,6 +1,5 @@
 #include "serial_port.h"
 
-
 // ----------------------------------------------------------------------------------
 //   Serial Port Manager Class
 // ----------------------------------------------------------------------------------
@@ -8,56 +7,56 @@
 // ------------------------------------------------------------------------------
 //   Con/De structors
 // ------------------------------------------------------------------------------
+namespace serial_led
+{
 Serial_Port::
-Serial_Port(const char *uart_name_ , int baudrate_)
+	Serial_Port(const char *uart_name_, int baudrate_)
 {
 	initialize_defaults();
 	uart_name = uart_name_;
-	baudrate  = baudrate_;
+	baudrate = baudrate_;
 }
 
 Serial_Port::
-Serial_Port(string url)
+	Serial_Port(string url)
 {
 	initialize_defaults();
 	uart_name = url.substr(url.find_last_of('/') + 1, url.find_last_of(':')).c_str();
-	baudrate  = stoi(url.substr(url.find_last_of(':') + 1, url.length()));
+	baudrate = stoi(url.substr(url.find_last_of(':') + 1, url.length()));
 }
 
 Serial_Port::
-Serial_Port()
+	Serial_Port()
 {
 	initialize_defaults();
 }
 
 Serial_Port::
-~Serial_Port()
+	~Serial_Port()
 {
 	// destroy mutex
 	pthread_mutex_destroy(&lock);
 }
 
-void
-Serial_Port::
-initialize_defaults()
+void Serial_Port::
+	initialize_defaults()
 {
 	// Initialize attributes
-	debug  = false;
-	fd     = -1;
+	debug = false;
+	fd = -1;
 	status = SERIAL_PORT_CLOSED;
 
-	uart_name = (char*)"/dev/ttyACM0";
-	baudrate  = 57600;
+	uart_name = (char *)"/dev/ttyACM0";
+	baudrate = 57600;
 
 	// Start mutex
 	int result = pthread_mutex_init(&lock, NULL);
-	if ( result != 0 )
+	if (result != 0)
 	{
 		printf("\n mutex init failed\n");
 		throw 1;
 	}
 }
-
 
 // ------------------------------------------------------------------------------
 //   Open Serial Port
@@ -65,9 +64,8 @@ initialize_defaults()
 /**
  * throws EXIT_FAILURE if could not open the port
  */
-void
-Serial_Port::
-open_serial()
+void Serial_Port::
+	open_serial()
 {
 
 	// --------------------------------------------------------------------------
@@ -114,46 +112,39 @@ open_serial()
 	printf("\n");
 
 	return;
-
 }
-
 
 // ------------------------------------------------------------------------------
 //   Close Serial Port
 // ------------------------------------------------------------------------------
-void
-Serial_Port::
-close_serial()
+void Serial_Port::
+	close_serial()
 {
 	printf("CLOSE PORT\n");
 
 	int result = close(fd);
 
-	if ( result )
+	if (result)
 	{
-		fprintf(stderr,"WARNING: Error on port close (%i)\n", result );
+		fprintf(stderr, "WARNING: Error on port close (%i)\n", result);
 	}
 
 	status = false;
 
 	printf("\n");
-
 }
-
 
 // ------------------------------------------------------------------------------
 //   Convenience Functions
 // ------------------------------------------------------------------------------
-void
-Serial_Port::
-start()
+void Serial_Port::
+	start()
 {
 	open_serial();
 }
 
-void
-Serial_Port::
-stop()
+void Serial_Port::
+	stop()
 {
 	close_serial();
 }
@@ -172,30 +163,28 @@ stop()
 // 	top();
 // }
 
-
 // ------------------------------------------------------------------------------
 //   Quit Handler
 // ------------------------------------------------------------------------------
-void
-Serial_Port::
-handle_quit( int sig )
+void Serial_Port::
+	handle_quit(int sig)
 {
-	try {
+	try
+	{
 		stop();
 	}
-	catch (int error) {
-		fprintf(stderr,"Warning, could not stop serial port\n");
+	catch (int error)
+	{
+		fprintf(stderr, "Warning, could not stop serial port\n");
 	}
 }
-
 
 // ------------------------------------------------------------------------------
 //   Helper Function - Open Serial Port File Descriptor
 // ------------------------------------------------------------------------------
 // Where the actual port opening happens, returns file descriptor 'fd'
-int
-Serial_Port::
-_open_port(const char* port)
+int Serial_Port::
+	_open_port(const char *port)
 {
 	// Open serial port
 	// O_RDWR - Read and write
@@ -206,7 +195,7 @@ _open_port(const char* port)
 	if (fd == -1)
 	{
 		/* Could not open the port. */
-		return(-1);
+		return (-1);
 	}
 
 	// Finalize
@@ -223,20 +212,19 @@ _open_port(const char* port)
 //   Helper Function - Setup Serial Port
 // ------------------------------------------------------------------------------
 // Sets configuration, flags, and baud rate
-bool
-Serial_Port::
-_setup_port(int baud, int data_bits, int stop_bits, bool parity, bool hardware_control)
+bool Serial_Port::
+	_setup_port(int baud, int data_bits, int stop_bits, bool parity, bool hardware_control)
 {
 	// Check file descriptor
-	if(!isatty(fd))
+	if (!isatty(fd))
 	{
 		fprintf(stderr, "\nERROR: file descriptor %d is NOT a serial port\n", fd);
 		return false;
 	}
 
 	// Read file descritor configuration
-	struct termios  config;
-	if(tcgetattr(fd, &config) < 0)
+	struct termios config;
+	if (tcgetattr(fd, &config) < 0)
 	{
 		fprintf(stderr, "\nERROR: could not read configuration of fd %d\n", fd);
 		return false;
@@ -256,15 +244,15 @@ _setup_port(int baud, int data_bits, int stop_bits, bool parity, bool hardware_c
 	// no Ctrl-D suppression, no fill characters, no case mapping,
 	// no local output processing
 	config.c_oflag &= ~(OCRNL | ONLCR | ONLRET |
-						 ONOCR | OFILL | OPOST);
+						ONOCR | OFILL | OPOST);
 
-	#ifdef OLCUC
-		config.c_oflag &= ~OLCUC;
-	#endif
+#ifdef OLCUC
+	config.c_oflag &= ~OLCUC;
+#endif
 
-	#ifdef ONOEOT
-		config.c_oflag &= ~ONOEOT;
-	#endif
+#ifdef ONOEOT
+	config.c_oflag &= ~ONOEOT;
+#endif
 
 	// No line processing:
 	// echo off, echo newline off, canonical mode off,
@@ -279,7 +267,7 @@ _setup_port(int baud, int data_bits, int stop_bits, bool parity, bool hardware_c
 
 	// One input byte is enough to return from read()
 	// Inter-character timer off
-	config.c_cc[VMIN]  = 1;
+	config.c_cc[VMIN] = 1;
 	config.c_cc[VTIME] = 10; // was 0
 
 	// Get the current options for the port
@@ -289,72 +277,72 @@ _setup_port(int baud, int data_bits, int stop_bits, bool parity, bool hardware_c
 	// Apply baudrate
 	switch (baud)
 	{
-		case 1200:
-			if (cfsetispeed(&config, B1200) < 0 || cfsetospeed(&config, B1200) < 0)
-			{
-				fprintf(stderr, "\nERROR: Could not set desired baud rate of %d Baud\n", baud);
-				return false;
-			}
-			break;
-		case 1800:
-			cfsetispeed(&config, B1800);
-			cfsetospeed(&config, B1800);
-			break;
-		case 9600:
-			cfsetispeed(&config, B9600);
-			cfsetospeed(&config, B9600);
-			break;
-		case 19200:
-			cfsetispeed(&config, B19200);
-			cfsetospeed(&config, B19200);
-			break;
-		case 38400:
-			if (cfsetispeed(&config, B38400) < 0 || cfsetospeed(&config, B38400) < 0)
-			{
-				fprintf(stderr, "\nERROR: Could not set desired baud rate of %d Baud\n", baud);
-				return false;
-			}
-			break;
-		case 57600:
-			if (cfsetispeed(&config, B57600) < 0 || cfsetospeed(&config, B57600) < 0)
-			{
-				fprintf(stderr, "\nERROR: Could not set desired baud rate of %d Baud\n", baud);
-				return false;
-			}
-			break;
-		case 115200:
-			if (cfsetispeed(&config, B115200) < 0 || cfsetospeed(&config, B115200) < 0)
-			{
-				fprintf(stderr, "\nERROR: Could not set desired baud rate of %d Baud\n", baud);
-				return false;
-			}
-			break;
-
-		// These two non-standard (by the 70'ties ) rates are fully supported on
-		// current Debian and Mac OS versions (tested since 2010).
-		case 460800:
-			if (cfsetispeed(&config, B460800) < 0 || cfsetospeed(&config, B460800) < 0)
-			{
-				fprintf(stderr, "\nERROR: Could not set desired baud rate of %d Baud\n", baud);
-				return false;
-			}
-			break;
-		case 921600:
-			if (cfsetispeed(&config, B921600) < 0 || cfsetospeed(&config, B921600) < 0)
-			{
-				fprintf(stderr, "\nERROR: Could not set desired baud rate of %d Baud\n", baud);
-				return false;
-			}
-			break;
-		default:
-			fprintf(stderr, "ERROR: Desired baud rate %d could not be set, aborting.\n", baud);
+	case 1200:
+		if (cfsetispeed(&config, B1200) < 0 || cfsetospeed(&config, B1200) < 0)
+		{
+			fprintf(stderr, "\nERROR: Could not set desired baud rate of %d Baud\n", baud);
 			return false;
+		}
+		break;
+	case 1800:
+		cfsetispeed(&config, B1800);
+		cfsetospeed(&config, B1800);
+		break;
+	case 9600:
+		cfsetispeed(&config, B9600);
+		cfsetospeed(&config, B9600);
+		break;
+	case 19200:
+		cfsetispeed(&config, B19200);
+		cfsetospeed(&config, B19200);
+		break;
+	case 38400:
+		if (cfsetispeed(&config, B38400) < 0 || cfsetospeed(&config, B38400) < 0)
+		{
+			fprintf(stderr, "\nERROR: Could not set desired baud rate of %d Baud\n", baud);
+			return false;
+		}
+		break;
+	case 57600:
+		if (cfsetispeed(&config, B57600) < 0 || cfsetospeed(&config, B57600) < 0)
+		{
+			fprintf(stderr, "\nERROR: Could not set desired baud rate of %d Baud\n", baud);
+			return false;
+		}
+		break;
+	case 115200:
+		if (cfsetispeed(&config, B115200) < 0 || cfsetospeed(&config, B115200) < 0)
+		{
+			fprintf(stderr, "\nERROR: Could not set desired baud rate of %d Baud\n", baud);
+			return false;
+		}
+		break;
 
-			break;
+	// These two non-standard (by the 70'ties ) rates are fully supported on
+	// current Debian and Mac OS versions (tested since 2010).
+	case 460800:
+		if (cfsetispeed(&config, B460800) < 0 || cfsetospeed(&config, B460800) < 0)
+		{
+			fprintf(stderr, "\nERROR: Could not set desired baud rate of %d Baud\n", baud);
+			return false;
+		}
+		break;
+	case 921600:
+		if (cfsetispeed(&config, B921600) < 0 || cfsetospeed(&config, B921600) < 0)
+		{
+			fprintf(stderr, "\nERROR: Could not set desired baud rate of %d Baud\n", baud);
+			return false;
+		}
+		break;
+	default:
+		fprintf(stderr, "ERROR: Desired baud rate %d could not be set, aborting.\n", baud);
+		return false;
+
+		break;
 	}
 
 	// Finally, apply the configuration
-	if(tcsetattr(fd, TCSAFLUSH, &config) < 0)
+	if (tcsetattr(fd, TCSAFLUSH, &config) < 0)
 	{
 		fprintf(stderr, "\nERROR: could not set configuration of fd %d\n", fd);
 		return false;
@@ -364,14 +352,11 @@ _setup_port(int baud, int data_bits, int stop_bits, bool parity, bool hardware_c
 	return true;
 }
 
-
-
 // ------------------------------------------------------------------------------
 //   Read Port with Lock
 // ------------------------------------------------------------------------------
-int
-Serial_Port::
-read_port(uint8_t &cp)
+int Serial_Port::
+	read_port(uint8_t &cp)
 {
 
 	// Lock
@@ -385,13 +370,11 @@ read_port(uint8_t &cp)
 	return result;
 }
 
-
 // ------------------------------------------------------------------------------
 //   Write Port with Lock
 // ------------------------------------------------------------------------------
-int
-Serial_Port::
-write_port(char *buf, unsigned len)
+int Serial_Port::
+	write_port(char *buf, unsigned len)
 {
 
 	// Lock
@@ -406,8 +389,7 @@ write_port(char *buf, unsigned len)
 	// Unlock
 	pthread_mutex_unlock(&lock);
 
-
 	return bytesWritten;
 }
 
-
+} // namespace serial_led
